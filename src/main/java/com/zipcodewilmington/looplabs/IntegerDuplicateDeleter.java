725b2281782e6acf6f1ad34101178de1e7ca4f81
Sandy Setiawan <ssetiawan@zipcodersMBP21.localdomain>
2020-03-10 10:27:46 -0400

    first commit

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -5,4 +5,66 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
+
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
+}
+
+    @Override
+    public Integer[] removeDuplicates(int maxNumberOfDuplications) {
+        int[] occurrences = occurrenceArray();
+        int removeCount = 0;
+        Integer[] temp = new Integer[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (occurrences[i] < maxNumberOfDuplications) {
+                temp[i] = array[i];
+            } else {
+                temp[i] = null;
+                removeCount++;
+            }
+        }
+        return copyArrayMinusNullValues(temp, new Integer[array.length-removeCount]);
+    }
+
+    @Override
+    public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int[] occurrences = occurrenceArray();
+        int removeCount = 0;
+        Integer[] temp = new Integer[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (occurrences[i] != exactNumberOfDuplications) {
+                temp[i] = array[i];
+            } else {
+                temp[i] = null;
+                removeCount++;
+            }
+        }
+        return copyArrayMinusNullValues(temp, new Integer[array.length-removeCount]);
+    }
+
+    private Integer[] copyArrayMinusNullValues(Integer[] temp, Integer[] result) {
+        int resultIndex = 0;
+        for (int j = 0; j < temp.length; j++) {
+            if (temp[j] != null) {
+                result[resultIndex] = temp[j];
+                resultIndex++;
+            }
+        }
+        return result;
+    }
+
+    private int[] occurrenceArray() {
+        int[] occurrences = new int[array.length];
+        int index = 0;
+        for (Integer i : this.array) {
+            occurrences[index] = numberOfOccurrences(i);
+            index++;
+        }
+        return occurrences;
+    }
+    private int numberOfOccurrences(Integer number) {
+        int count = 0;
+        for (Integer i : this.array) if (i.equals(number)) count++;
+        return count;
+    }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -5,4 +5,67 @@ package com.zipcodewilmington.looplabs;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
+    public StringDuplicateDeleter(String[] intArray) {
+        super(intArray);
+    }
+
+    @Override
+    public String[] removeDuplicates(int maxNumberOfDuplications) {
+        int[] occurrences = occurrenceArray();
+        int removeCount = 0;
+        String[] temp = new String[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (occurrences[i] < maxNumberOfDuplications) {
+                temp[i] = array[i];
+            } else {
+                temp[i] = null;
+                removeCount++;
+            }
+        }
+        return copyArrayMinusNullValues(temp, new String[array.length-removeCount]);
+    }
+
+    @Override
+    public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
+        int[] occurrences = occurrenceArray();
+        int removeCount = 0;
+        String[] temp = new String[array.length];
+        for (int i = 0; i < array.length; i++) {
+            if (occurrences[i] != exactNumberOfDuplications) {
+                temp[i] = array[i];
+            } else {
+                temp[i] = null;
+                removeCount++;
+            }
+        }
+        return copyArrayMinusNullValues(temp, new String[array.length-removeCount]);
+    }
+
+    private String[] copyArrayMinusNullValues(String[] temp, String[] result) {
+        int resultIndex = 0;
+        for (int j = 0; j < temp.length; j++) {
+            if (temp[j] != null) {
+                result[resultIndex] = temp[j];
+                resultIndex++;
+            }
+        }
+        return result;
+    }
+
+    private int[] occurrenceArray() {
+        int[] occurrences = new int[array.length];
+        int index = 0;
+        for (String str : this.array) {
+            occurrences[index] = numberOfOccurrences(str);
+            index++;
+        }
+        return occurrences;
+    }
+
+    private int numberOfOccurrences(String string) {
+        int count = 0;
+        for (String str : this.array) if (str.equals(string)) count++;
+        return count;
+    }
 }
+
